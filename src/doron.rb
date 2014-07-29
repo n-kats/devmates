4e709f466bb529d2af6d9c5227aa6ed15dae580c
n-kats <n-kats19890214@hotmail.co.jp>
2014-07-30 00:30:01 +0900

    before bundle gem

--- src/doron.rb
+++ src/doron.rb
@@ -1,4 +1,17 @@
 # coding : UTF-8
+require 'thor'
 
-
-
+class Doron < Thor
+  
+  desc 'hello', 'say hello message'
+  def hello
+    puts '‚¿‚Á‚·'
+  end
+end
+=begin
+puts Thor.methods.select{|x|!(Object.methods.include?x)}
+puts "--"
+puts Thor.instance_methods.select{|x|!(Object.instance_methods.include?x)}
+puts "--"
+=end
+Doron.start(ARGV)
