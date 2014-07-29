141f41edcfd860d4482ec368ad07e4207da09c80
n-kats <n-kats19890214@hotmail.co.jp>
2014-07-30 06:19:48 +0900

    make doron.rb\n\nI made doron.rb

--- src/doron.rb
+++ src/doron.rb
@@ -1,17 +1,24 @@
 # coding : UTF-8
 require 'thor'
+require 'yaml'
 
 class Doron < Thor
   
-  desc 'hello', 'say hello message'
-  def hello
-    puts "ちっす"
+  hashes = YAML.load(File.read "./doron_user.yml")
+  
+  hashes.each do |cmd,h|
+    desc cmd, "#{h["name"]}の設定にする"
+    define_method(cmd) do
+      apply_config h
+    end
   end
+
+  private
+    def apply_config(h)
+      h.each do |k,v|
+        `git config --global user.#{k} "#{v}"`
+      end
+    end
 end
-=begin
-puts Thor.methods.select{|x|!(Object.methods.include?x)}
-puts "--"
-puts Thor.instance_methods.select{|x|!(Object.instance_methods.include?x)}
-puts "--"
-=end
+
 Doron.start(ARGV)
